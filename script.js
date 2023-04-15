5bcf455fa2a89788b4fb21eb976ca467e3335850
Aditya Agarwal <agarwaladitya27257@outlook.com>
2023-04-15 11:50:26 +0530

    some changes

--- script.js
+++ script.js
@@ -11,95 +11,17 @@ var firebaseConfig = {
   
   // // Initialize Firebase
    firebase.initializeApp(firebaseConfig);
-  //  var dbRef = firebase.database().ref("nodemcu1")
-  //  dbRef.once("value",function(snapshot){
-  //   var data = snapshot;
-  //       console.log(data.val());
-  //      console.log(data.val().slot1)
-  // })
-   $(document).ready(function(){
-   var database = firebase.database();
-   var p1,p2,p3,p4;
-  
-     database.ref().on("value", function(snap){
-     p1 = snap.val().p1;
-     if(p1 == 1){
-       $(".slotstatus1").text("Available");
-     } else {
-        $(".slotstatus1").text("Not Available");
-      }
-      p2 = snap.val().p2;
-      if(p2 == 1){
-       $(".slotstatus2").text("Available");
-       $(".slotstatus2").text("Not Available");
-    }
-     p3 = snap.val().p3;
-      if(p3 == 1){
-      $(".slotstatus3").text("Available");
-    } else {
-      $(".slotstatus3").text("Not Available");
-    }
-      p4 = snap.val().p4;
-     if(p4 == 1){
-       $(".slotstatus4").text("Available");
-     } else {
-       $(".slotstatus4").text("Not Available");
-     }
-    });
-//});
-$("#button1").click(function(){
-  var firebaseRef = firebase.database().ref().child("slotnumber");
-  if(p1 == 1){
-    location.href = "map1.html";
-    firebaseRef.set(1);
-  } else {
-    location.href = "#";
-  }
-});
-$("#button2").click(function(){
-  var firebaseRef = firebase.database().ref().child("slotnumber");
-  if(p2 == 1){
-    firebaseRef.set(2);
-    location.href = "map1.html";
-  } else {
-    location.href = "#";
-  }
-});
-$("#button3").click(function(){
-  var firebaseRef = firebase.database().ref().child("slotnumber");
-  if(p3 == 1){
-    firebaseRef.set(3);
-    location.href = "map1.html";
-  } else {
-    location.href = "#";
-  }
-});
-$("#button4").click(function(){
-  var firebaseRef = firebase.database().ref().child("slotnumber");
-  if(p4 == 0){
-    firebaseRef.set(4);
-    location.href = "map1.html";
-  } else {
-    location.href = "#";
-  }
-});
-// $("#button5").click(function(){
-//   var firebaseRef = firebase.database().ref().child("slotnumber");
-//   if(p5 == 1){
-//     firebaseRef.set(5);
-//     location.href = "map1.html";
-//   } else {
-//     location.href = "#";
-//   }
-// });
-// $("#button6").click(function(){
-//   var firebaseRef = firebase.database().ref().child("slotnumber");
-//   if(p6 == 1){
-//     firebaseRef.set(6);
-//     location.href = "map1.html";
-//   } else {
-//     location.href = "#";
-//   }
-// });
+   let slots
+   var dbRef = firebase.database().ref("nodemcu1")
+   setInterval(() => {
     
-});
+     dbRef.once("value",function(snapshot){
+       var data = snapshot;
+       slots = data.val();
+      //  console.log(data.val());
+      //  console.log(data.val().slot1)
+      })
+      console.log(slots)
+      
+    }, 1000);
+
